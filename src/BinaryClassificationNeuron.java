94c40a46c2c7b7eb5117c0fc2fa8fccd87a81f07
jf17 <javafirst17@gmail.com>
2018-07-06 14:55:17 +0300

    create BinaryClassificationNeuron

---
+++ src/BinaryClassificationNeuron.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.LongAdder;
+/*
+* @author  Alexandrovich Alexey Borisovich
+*/
+
+public class BinaryClassificationNeuron {
+
+    final Map<String, LongAdder> myMap;
+
+    public BinaryClassificationNeuron() {
+        myMap = new HashMap<String, LongAdder>();
+    }
+
+    public void put(String key, boolean binaryClassificator) {
+
+        if (myMap.containsKey(key) && binaryClassificator == true) {
+            myMap.get(key).increment();
+        } else if (myMap.containsKey(key) && binaryClassificator == false) {
+            myMap.get(key).decrement();
+        } else {
+            myMap.put(key, new LongAdder());
+        }
+
+    }
+
+    public void print() {
+
+        System.out.println(myMap);
+
+    }
+
+}
